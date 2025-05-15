403502b2a6600352ab2ab3ffb0e47612831c565b
victoria Fakunle <v.fakunle@alustudent.com>
2025-05-15 13:42:11 +0200

    linegraph

---
+++ math/plotting/0-line.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = np.arange(0, 11) ** 3
+
+plt.plot(np.arange(0, 11), y, 'r-')
+
+plt.xlim(0, 10)
+plt.ylim(0, 1000)
+
+plt.show()

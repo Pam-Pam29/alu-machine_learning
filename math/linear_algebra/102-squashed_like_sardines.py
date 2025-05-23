a09e56090907f87bb5b28a6dabcacd567aeec7da
Victoria Fakunle <v.fakunle@alustudent.com>
2025-05-23 13:19:50 +0200

    Update 102-squashed_like_sardines.py

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python3
-'''
-This is a simple script that demonstrates the use of the numpy library to solve a system of linear equations.
-'''
-import numpy as np
+"""
+Concatenates two matrices along a specific axis
+"""
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    '''
-    Concatenates two matrices along a specific axis
-    '''
-    try:
-        arr1 = np.array(mat1)
-        arr2 = np.array(mat2)
-        
-        result = np.concatenate((arr1, arr2), axis=axis)
-        
-        return result.tolist()
-    except ValueError:
+    from copy import deepcopy
+
+    def shape(matrix):
+        """Recursively determines the shape of a matrix"""
+        if not isinstance(matrix, list):
+            return []
+        return [len(matrix)] + shape(matrix[0])
+
+    def is_shape_compatible(s1, s2, axis):
+        """Check if two shapes are compatible for concatenation"""
+        if len(s1) != len(s2):
+            return False
+        for i in range(len(s1)):
+            if i != axis and s1[i] != s2[i]:
+                return False
+        return True
+
+    s1 = shape(mat1)
+    s2 = shape(mat2)
+
+    if not is_shape_compatible(s1, s2, axis):
         return None
+
+    def concat(m1, m2, level=0):
+        """Recursively concatenate matrices"""
+        if level == axis:
+            return deepcopy(m1) + deepcopy(m2)
+        return [concat(m1[i], m2[i], level + 1) for i in range(len(m1))]
+
+    return concat(mat1, mat2)

49b145665433cc34949c0e319e8e15c130e2a061
Victoria Fakunle <v.fakunle@alustudent.com>
2025-05-23 12:54:10 +0200

    Update 101-the_whole_barn.py

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''
 This is a simple script that demonstrates the use
-of the numpy library to solve a system of linear equations. | Python | Shobi
+of the numpy library to solve a system of linear equations. | Python | PamPam
 '''
 
 

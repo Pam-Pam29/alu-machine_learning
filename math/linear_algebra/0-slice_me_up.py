ea15b5ea8b46e1fd31fafa3f451536e4f0e6da5b
victoria Fakunle <v.fakunle@alustudent.com>
2025-05-23 11:19:02 +0200

    matrix

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
 arr1 = arr[:2]
@@ -7,13 +6,3 @@ arr3 = arr[1:6]
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
-=======
-#!/usr/bin/env python3
-arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 = arr[:2]
-arr2 = arr[-5:]
-arr3 = arr[1:6]
-print("The first two numbers of the array are: {}".format(arr1))
-print("The last five numbers of the array are: {}".format(arr2))
-print("The 2nd through 6th numbers of the array are: {}".format(arr3))
->>>>>>> eab8c09c4c9216119825de91792106c35f620d96
